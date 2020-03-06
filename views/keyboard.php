16fe2e232bd6cc5e9dcbc378e97c3a267765ff38
chapmanCC <chapboy1989@gmail.com>
2020-03-06 14:25:18 +0800

    update views/ -- delete comments and reordering attributes

--- views/keyboard.php
+++ views/keyboard.php
@@ -1,70 +1,36 @@
 <div id="qwerty" class="section">
     <form action="play.php" method="post">
         <div class="keyrow">
-            <input class="key" type="submit" value="q" name="key">
-            <input class="key" type="submit" value="w" name="key">
-            <input class="key" type="submit" value="e" name="key">
-            <input class="key" type="submit" value="r" name="key">
-            <input class="key" type="submit" value="t" name="key">
-            <input class="key" type="submit" value="y" name="key">
-            <input class="key" type="submit" value="u" name="key">
-            <input class="key" type="submit" value="i" name="key">
-            <input class="key" type="submit" value="o" name="key">
-            <input class="key" type="submit" value="p" name="key">
+            <input class="key" type="submit" name="key" value="q">
+            <input class="key" type="submit" name="key" value="w">
+            <input class="key" type="submit" name="key" value="e">
+            <input class="key" type="submit" name="key" value="r">
+            <input class="key" type="submit" name="key" value="t">
+            <input class="key" type="submit" name="key" value="y">
+            <input class="key" type="submit" name="key" value="u">
+            <input class="key" type="submit" name="key" value="i">
+            <input class="key" type="submit" name="key" value="o">
+            <input class="key" type="submit" name="key" value="p">
         </div>
         <div class="keyrow">
-            <input type="submit" value="a" class="key" name="key">
-            <input type="submit" value="s" class="key" name="key">
-            <input type="submit" value="d" class="key" name="key">
-            <input type="submit" value="f" class="key" name="key">
-            <input type="submit" value="g" class="key" name="key">
-            <input type="submit" value="h" class="key" name="key">
-            <input type="submit" value="j" class="key" name="key">
-            <input type="submit" value="k" class="key" name="key">
-            <input type="submit" value="l" class="key" name="key">
+            <input type="submit" value="a" clname="key" ass="key">
+            <input type="submit" value="s" clname="key" ass="key">
+            <input type="submit" value="d" clname="key" ass="key">
+            <input type="submit" value="f" clname="key" ass="key">
+            <input type="submit" value="g" clname="key" ass="key">
+            <input type="submit" value="h" clname="key" ass="key">
+            <input type="submit" value="j" clname="key" ass="key">
+            <input type="submit" value="k" clname="key" ass="key">
+            <input type="submit" value="l" clname="key" ass="key">
         </div>
         <div class="keyrow">
-            <input type="submit" value="z" class="key" name="key">
-            <input type="submit" value="x" class="key" name="key">
-            <input type="submit" value="c" class="key" name="key">
-            <input type="submit" value="v" class="key" name="key">
-            <input type="submit" value="b" class="key" name="key">
-            <input type="submit" value="n" class="key" name="key">
-            <input type="submit" value="m" class="key" name="key">
+            <input type="submit" value="z" clname="key" ass="key">
+            <input type="submit" value="x" clname="key" ass="key">
+            <input type="submit" value="c" clname="key" ass="key">
+            <input type="submit" value="v" clname="key" ass="key">
+            <input type="submit" value="b" clname="key" ass="key">
+            <input type="submit" value="n" clname="key" ass="key">
+            <input type="submit" value="m" clname="key" ass="key">
         </div>
     </form>
-    <!-- <div class="keyrow">
-        <button class="key">q</button>
-        <button class="key">w</button>
-        <button class="key">e</button>
-        <button class="key">r</button>
-        <button class="key" style="background-color: red" disabled>t</button>
-        <button class="key">y</button>
-        <button class="key">u</button>
-        <button class="key">i</button>
-        <button class="key">o</button>
-        <button class="key">p</button>
-    </div>
-
-    <div class="keyrow">
-        <button class="key">a</button>
-        <button class="key">s</button>
-        <button class="key">d</button>
-        <button class="key">f</button>
-        <button class="key">g</button>
-        <button class="key">h</button>
-        <button class="key">j</button>
-        <button class="key">k</button>
-        <button class="key">l</button>
-    </div>
-
-    <div class="keyrow">
-        <button class="key">z</button>
-        <button class="key">x</button>
-        <button class="key">c</button>
-        <button class="key">v</button>
-        <button class="key">b</button>
-        <button class="key">n</button>
-        <button class="key">m</button>
-    </div> -->
 </div>
--- views/phrase.php
+++ views/phrase.php
@@ -3,16 +3,5 @@
         <?php
         echo $game->phrase->addPhraseToDisplay();
         ?>
-        <!-- <li class="hide letter h">h</li>
-        <li class="hide letter o">o</li>
-        <li class="hide letter w">w</li>
-        <li class="hide space"> </li>
-        <li class="hide letter a">a</li>
-        <li class="hide letter r">r</li>
-        <li class="hide letter e">e</li>
-        <li class="hide space"> </li>
-        <li class="hide letter y">y</li>
-        <li class="hide letter o">o</li>
-        <li class="hide letter u">u</li> -->
     </ul>
 </div>
