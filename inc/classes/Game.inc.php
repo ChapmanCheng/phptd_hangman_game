1554773c651709918f0135d045c3689a2c425f73
chapmanCC <chapboy1989@gmail.com>
2020-03-06 16:36:14 +0800

    added Game.inc.php required methods

--- inc/classes/Game.inc.php
+++ inc/classes/Game.inc.php
@@ -3,7 +3,33 @@
 	private $phrase;
 	private $lives;
 
-	// showing the game, 
-	// handling interactions, 
-	// checking game is over
+	public function __construct(Phrase $phrase)
+	{
+		$this->phrase = $phrase;
+	}
+	public function __get($name)
+	{
+		if (isset($this->$name))
+			return $this->$name;
+	}
+	public function __set($name, $value)
+	{
+		if (isset($this->name))
+			$this->$name = $value;
+	}
+	public function checkForWin()
+	{
+	}
+	public function checkForLose()
+	{
+	}
+	public function gameOver()
+	{
+	}
+	public function displayKeyboard()
+	{
+	}
+	public function displayScore()
+	{
+	}
 }
